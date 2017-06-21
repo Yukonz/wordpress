1d1f640c40d235b8abaa2082366b8257730fd53d
dev70 <wiktor.g@live.com>
2017-06-21 15:49:57 +0300

    Fix AJAX Edit function bugs

--- wp-content/plugins/wp_mymap/js/edit_tweet.js
+++ wp-content/plugins/wp_mymap/js/edit_tweet.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function($){
+    var id, name, text, date, currentTweet;
+
     $('a.edit_link').on('click', function(event){
+        id = $(this).closest('tr').find('span.tweet-id').html();
+        name = $(this).closest('tr').find('span.tweet-name').html();
+        text = $(this).closest('tr').find('span.tweet-text').html();
+        date = $(this).closest('tr').find('span.tweet-date').html();
 
-        var id = $(this).closest('tr').find('span.tweet-id').html();
-        var name = $(this).closest('tr').find('span.tweet-name').html();
-        var text = $(this).closest('tr').find('span.tweet-text').html();
-        var date = $(this).closest('tr').find('span.tweet-date').html();
-
-        var currentTweet = this;
+        currentTweet = $(this);
 
         $( "#edit_form" ).fadeIn(300);
 
@@ -14,29 +15,28 @@ jQuery(document).ready(function($){
         $("#edit_date").val(date);
         $("#edit_text").val(text);
 
-        $('#edit_button').on('click', function(){
-            var data = {
-                action: 'edit_tweet',
-                edit_name: $("#edit_name").val(),
-                edit_date: $("#edit_date").val(),
-                edit_text: $("#edit_text").val(),
-                edit_id: id
-            };
-
-            $.post( ajaxurl, data, function() {
-                name = $("#edit_name").val();
-                date = $("#edit_date").val();
-                text = $("#edit_text").val();
-
-                $(currentTweet).closest('tr').find('.tweet-name').html(name);
-                $(currentTweet).closest('tr').find('.tweet-text').html(text);
-                $(currentTweet).closest('tr').find('.tweet-date').html(date);
-
-                $( "#edit_form" ).fadeOut(300);
-
-            });
-
-        });
         event.preventDefault();
+    });
+
+    $('#edit_button').on('click', function(){
+        var data = {
+            action: 'edit_tweet',
+            edit_name: $("#edit_name").val(),
+            edit_date: $("#edit_date").val(),
+            edit_text: $("#edit_text").val(),
+            edit_id: id
+        };
+
+        $.post( ajaxurl, data, function() {
+            name = $("#edit_name").val();
+            date = $("#edit_date").val();
+            text = $("#edit_text").val();
+
+            $(currentTweet).closest('tr').find('.tweet-name').html(name);
+            $(currentTweet).closest('tr').find('.tweet-text').html(text);
+            $(currentTweet).closest('tr').find('.tweet-date').html(date);
+
+            $( "#edit_form" ).fadeOut(300);
+        });
     });
 });
